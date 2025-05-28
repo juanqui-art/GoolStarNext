7240101d94c589c3881304f259ce3f1a03dff79c
juanqui <juanquizh@gmail.com>
2025-05-28 14:44:30 -0500

    Add ESLint configuration for Next.js and update dependencies and `eslint-plugin-react-hooks` to enhance linting rules. Updated `eslint.config.mjs` to include Next.js-specific configurations and prepared the project for improved code quality and consistency.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,30 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import nextPlugin from "@next/eslint-plugin-next";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {
+    plugins: {
+      "@next/next": nextPlugin
+    }
+  }
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "@next/next": nextPlugin
+    },
+    rules: {
+      // Add any custom rules here
+    }
+  }
 ];
 
 export default eslintConfig;
